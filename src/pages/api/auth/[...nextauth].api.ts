a8732de3428815f4e76a7700bfadcdde1155850f
Alexandre Junior <alexandrejunior408@gmail.com>
2023-01-06 16:33:21 -0300

    feat: update

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,34 +1,44 @@
 import { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth/next'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '../../../lib/next-auth/prisma-adapter'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+export const buildNextAuthOptions(): NextAuthOptions  {
+  return {
+    adapter: PrismaAdapter(),
+
+    // Configure one or more authentication providers
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+          },
         },
+      }),
+      // ...add more providers here
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions'
+        }
+  
+        // se tiver o escopo necessário irá retornar true o if já serve para fazer essa checagem e redirecionar para outra página
+        return true
       },
-    }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions'
-      }
-
-      // se tiver o escopo necessário irá retornar true o if já serve para fazer essa checagem e redirecionar para outra página
-      return true
     },
-  },
+  }
+ 
 }
 
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, authOptions)
+}
